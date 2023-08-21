8360e7a1f6aa566a181cf81c5cb2186e6656b976
Ezequiel Frías <ezequiel@3dlinkweb.com>
2023-08-21 17:38:01 -0400

    feat: Cambios

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "https://3dlinkweb.gitlab.io/payment-form/"
+  plugins: [react()]
 })
