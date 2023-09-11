15a74732050a70e79cc12fe20cd62ca8dd01c577
Ezequiel Frías <ezequiel@3dlinkweb.com>
2023-09-11 17:29:56 -0400

    feat: Se agrego video-link

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,14 +1,16 @@
 import {
-    createBrowserRouter,
-    RouterProvider,
-    Route,
-    Link,
+    createBrowserRouter
   } from "react-router-dom";
 import Payment from "./pages/payment/Payment";
+import VideoLink from "./pages/video-link/VideoLink";
 
   export const router = createBrowserRouter([
     {
       path: "/comprafinalizada/:control",
       element: <Payment />,
+    },
+    {
+      path: "/video-link",
+      element: <VideoLink />,
     },
   ]);
---
+++ src/pages/video-link/VideoLink.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const VideoLink = () => {
+  return (
+    <div style={{
+        flex: 1
+    }}>
+    <div class="vl-container" style={{
+        flex: 1
+    }}>
+      <data-videolink
+        token="0639eabe1cc6dc8bb531b5b8e5f0a97e46dc224580471fc0a6d78224c295f052"
+      >
+      </data-videolink>
+    </div>
+    </div>
+  )
+}
+
+export default VideoLink
