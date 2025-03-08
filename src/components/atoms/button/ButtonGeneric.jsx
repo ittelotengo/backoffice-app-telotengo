bf0d6cac1c6051365aba2f686bab9bf23d3e6c27
rcristancho <ramoncristancho@outlook.com>
2025-03-07 22:31:50 -0400

    Remove wrong font in the Eliminar button

--- src/components/atoms/button/ButtonGeneric.jsx
+++ src/components/atoms/button/ButtonGeneric.jsx
@@ -16,17 +16,17 @@ const ButtonGeneric = ({
         disabled
           ? "bg-gray_button_disabled border-gray_button_disabled"
           : "bg-primary border-primary text-white"
-      } px-4 py-3 border-2  rounded-sm h-full font-montserrat font-semibold ${className}`}
+      } px-4 py-3 border-2  rounded-sm h-full font-semibold ${className}`}
       onClick={onCLick}
       disabled={disabled}
       {...props}
     >
       {text}
-    </button>
+    </button> 
   ) : (
     <button
       type={type}
-      className={`bg-white px-4 py-3 rounded-sm h-full  border-2 border-primary text-primary rounderd-sm font-montserrat font-semibold ${className}`}
+      className={`bg-white px-4 py-3 rounded-sm h-full  border-2 border-primary text-primary rounderd-sm font-semibold ${className}`}
       onClick={onCLick}
       {...props}
     >
